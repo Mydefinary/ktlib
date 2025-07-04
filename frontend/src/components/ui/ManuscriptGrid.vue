ce50e97ac0696bf9bc7bceb1275f177678862dcd
My refinary <kimhoa90@naver.com>
2025-07-04 06:43:19 +0000

    최최최최최종

--- frontend/src/components/ui/ManuscriptGrid.vue
+++ frontend/src/components/ui/ManuscriptGrid.vue
@@ -1,50 +1,101 @@
 <template>
 <v-container>
-<!-- 등록/수정 버튼 -->
-<div style="margin-bottom: 16px;">
-<v-btn color="primary" @click="openAdd">등록</v-btn>
-<v-btn color="primary" :disabled="!selected" @click="openEdit">수정</v-btn>
+<!-- Snackbar -->
+<v-snackbar v-model="snackbar.status" :timeout="snackbar.timeout" :color="snackbar.color">
+<v-btn style="margin-left: 80px;" text @click="snackbar.status = false">Close</v-btn>
+</v-snackbar>
+ 
+    <!-- 버튼 영역 -->
+<div class="panel">
+<div class="gs-bundle-of-buttons" style="max-height:10vh;">
+<v-btn @click="addNewRow" class="contrast-primary-text" small color="primary">
+<v-icon small style="margin-left: -5px;">mdi-plus</v-icon>등록
+</v-btn>
+<v-btn :disabled="!selectedRow" style="margin-left: 5px;" @click="openEditDialog" class="contrast-primary-text" small color="primary">
+<v-icon small>mdi-pencil</v-icon>수정
+</v-btn>
+<v-btn :disabled="!selectedRow" style="margin-left: 5px;" color="green" small @click="publishbook">
+          출간요청
+</v-btn>
 </div>
  
-    <!-- 테이블 -->
+      <!-- 목록 -->
+<ManuscriptList @search="search" style="margin-bottom: 10px; background-color: #ffffff;" />
+ 
+      <!-- 테이블 -->
+<div class="table-responsive">
 <v-table>
 <thead>
 <tr>
-<th>ID</th>
-<th>제목</th>
-<th>내용</th>
+<th>Id</th>
+<th>Title</th>
+<th>Content</th>
+<th>Status</th>
+<th>Author</th>
+<th>Updated At</th>
 </tr>
 </thead>
 <tbody>
 <tr
-          v-for="(item, index) in manuscripts"
-          :key="index"
-          :style="item === selected ? 'background: #eef;' : ''"
-          @click="selected = item"
+              v-for="(val, idx) in value"
+              :key="val"
+              :style="val === selectedRow ? 'background-color: rgb(var(--v-theme-primary), 0.2) !important;' : ''"
+              @click="changeSelectedRow(val)"
 >
-<td>{{ index + 1 }}</td>
-<td>{{ item.title }}</td>
-<td>{{ item.content.length > 100 ? item.content.slice(0, 100) + '...' : item.content }}</td>
+<td>{{ idx + 1 }}</td>
+<td>{{ val.title }}</td>
+<td style="max-width: 250px;" class="text-truncate">
+                {{ val.content.length > 100 ? val.content.slice(0, 100) + '...' : val.content }}
+</td>
+<td>{{ val.status || '-' }}</td>
+<td>{{ val.authorNickname }} (#{{ val.authorId }})</td>
+<td>{{ val.lastModified || val.createdDate }}</td>
+<v-row class="ma-0 pa-4 align-center">
+<v-spacer></v-spacer>
+<Icon style="cursor: pointer;" icon="mi:delete" @click.stop="deleteRow(val)" />
+</v-row>
 </tr>
 </tbody>
 </v-table>
+</div>
+</div>
  
     <!-- 등록 다이얼로그 -->
-<v-dialog v-model="showAdd" width="500">
+<v-dialog v-model="openDialog" transition="dialog-bottom-transition" width="40%">
 <v-card>
-<v-card-title>원고 등록</v-card-title>
+<v-toolbar color="primary" class="elevation-0 pa-4" height="50px">
+<div style="color:white; font-size:17px; font-weight:700;">Manuscript 등록</div>
+<v-spacer></v-spacer>
+<v-icon color="white" small @click="closeDialog()">mdi-close</v-icon>
+</v-toolbar>
 <v-card-text>
-<Manuscript :initial-value="emptyManuscript()" :isNew="true" @add="addManuscript" />
+<Manuscript
+            :value="newValue"
+            :editMode="true"
+            :isNew="true"
+            @update:value="v => newValue = v"
+            @add="append"
+          />
 </v-card-text>
 </v-card>
 </v-dialog>
  
     <!-- 수정 다이얼로그 -->
-<v-dialog v-model="showEdit" width="500">
+<v-dialog v-model="editDialog" transition="dialog-bottom-transition" width="40%">
 <v-card>
-<v-card-title>원고 수정</v-card-title>
-<v-card-text>
-<Manuscript :initial-value="selected" :isNew="false" @save="updateManuscript" />
+<v-toolbar color="primary" class="elevation-0 pa-4" height="50px">
+<div style="color:white; font-size:17px; font-weight:700;">Manuscript 수정</div>
+<v-spacer></v-spacer>
+<v-icon color="white" small @click="closeDialog()">mdi-close</v-icon>
+</v-toolbar>
+<v-card-text class="scroll-wrapper">
+<Manuscript
+            :value="editingRow"
+            :editMode="true"
+            :isNew="false"
+            @update:value="v => editingRow = v"
+            @save="saveEdit"
+          />
 </v-card-text>
 </v-card>
 </v-dialog>
@@ -52,46 +103,84 @@
 </template>
  
 <script>
-import Manuscript from './Manuscript.vue'
+import axios from 'axios';
+import BaseGrid from '../base-ui/BaseGrid.vue';
+import ManuscriptRepository from '../repository/ManuscriptRepository';
+import Manuscript from '../Manuscript.vue';
  
 export default {
+  name: 'manuscriptGrid',
+  mixins: [BaseGrid],
   components: { Manuscript },
-  data() {
-    return {
-      manuscripts: [],
-      selected: null,
-      showAdd: false,
-      showEdit: false,
-    }
+  data: () => ({
+    path: 'manuscripts',
+    openDialog: false,
+    editDialog: false,
+    newValue: {},
+    editingRow: {}
+  }),
+  created() {
+    this.repository = new ManuscriptRepository(axios);
+    this.search();
   },
   methods: {
-    openAdd() {
-      this.showAdd = true
-    },
-    openEdit() {
-      this.showEdit = true
-    },
-    addManuscript(newItem) {
-      this.manuscripts.push(newItem)
-      this.showAdd = false
-    },
-    updateManuscript(updatedItem) {
-      const index = this.manuscripts.indexOf(this.selected)
-      if (index !== -1) {
-        this.manuscripts.splice(index, 1, updatedItem)
-        this.selected = updatedItem
+    async publishbook() {
+      if (!this.selectedRow) return;
+      try {
+        if (this.selectedRow.status === 'RequestedPublish') {
+          throw '이미 처리된 요청입니다';
+        }
+        const id = this.selectedRow._links.self.href.split('/').pop();
+        await this.repository.publishBook(id);
+        this.success('출간 요청 되었습니다.');
+        await this.search();
+      } catch (e) {
+        this.error(e);
       }
-      this.showEdit = false
     },
-    emptyManuscript() {
-      return {
+ 
+    async saveEdit(updatedData) {
+      try {
+        updatedData.lastModified = new Date().toISOString();
+        updatedData.status = 'MODIFIED';
+        await this.repository.save(updatedData, false);
+        this.success('수정 완료');
+        this.editDialog = false;
+        await this.search();
+      } catch (e) {
+        this.error(e);
+      }
+    },
+ 
+    addNewRow() {
+      this.newValue = {
         title: '',
         content: '',
+        authorId: null,
+        authorNickname: '',
         createdDate: new Date().toISOString(),
-        lastModified: null,
         status: 'DRAFT'
-      }
+      };
+      this.openDialog = true;
+    },
+ 
+    openEditDialog() {
+      this.editingRow = { ...this.selectedRow };
+      this.editDialog = true;
     }
   }
-}
+};
 </script>
+ 
+<style scoped>
+.text-truncate {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.scroll-wrapper {
+  max-height: 75vh;
+  overflow-y: auto;
+  padding: 12px;
+}
+</style>
